9860152dd5ce34bba2d81a74dc5377992cefb58d
Srikant Patnaik <srikant@gnu-linux.org>
2014-08-25 12:11:15 +0530

    changed mount file systems

--- androlinux_xorg.sh
+++ androlinux_xorg.sh
@@ -4,10 +4,15 @@
 # This step will stop android UI
 setprop ctl.stop media & setprop ctl.stop zygote & setprop ctl.stop surfaceflinger & setprop ctl.stop drm
 
+export PATH=$bin:/usr/bin:/usr/local/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/games:$PATH
+export TERM=linux
+
 chroot_path="/data/linux"
 export HOME=/root
 export HOSTNAME=netbook
 export DISPLAY=:0
+
+
 
 # Change the path with your root file system (it can be any distro, doesn't matter)
 mount /storage/sdcard1/14.04.1_rootfs.img $chroot_path
@@ -15,12 +20,15 @@ mount /storage/sdcard1/14.04.1_rootfs.img $chroot_path
 start_mount()
 	{
         if [ ! -f "$chroot_path/proc/uptime" ]; then
-                mount --bind /proc $chroot_path/proc
+               #mount --bind /proc $chroot_path/proc
+		busybox mount -t proc proc /proc $chroot_path/proc
         fi
 
         if [ ! -f "$chroot_path/dev/random" ]; then
-                mount --bind /dev $chroot_path/dev
-                mount --bind /dev/pts $chroot_path/dev/pts
+                #mount --bind /dev $chroot_path/dev
+	        busybox mount -t devtmpfs devtmpfs /dev $chroot_path/dev
+               # mount --bind /dev/pts $chroot_path/dev/pts
+	        busybox mount -t devpts devpts /dev/pts $chroot_path/dev/pts
 		ln -n /dev/graphics/fb0 /dev/fb0
         fi
 
