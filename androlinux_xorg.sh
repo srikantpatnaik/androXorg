573e8920edfc48d3965c7766078f6cbace57d487
Srikant Patnaik <srikant@gnu-linux.org>
2014-08-25 14:56:45 +0530

    rearranged functions

--- androlinux_xorg.sh
+++ androlinux_xorg.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 
-# To run this script, first ssh to Android using sshdroid or similar utility.
 # This step will stop android UI
 setprop ctl.stop media & setprop ctl.stop zygote & setprop ctl.stop surfaceflinger & setprop ctl.stop drm
 
@@ -12,40 +11,25 @@ export HOME=/root
 export HOSTNAME=netbook
 export DISPLAY=:0
 
-
-
 # Change the path with your root file system (it can be any distro, doesn't matter)
 mount /storage/sdcard1/14.04.1_rootfs.img $chroot_path
 
-start_mount()
-	{
-        if [ ! -f "$chroot_path/proc/uptime" ]; then
-               #mount --bind /proc $chroot_path/proc
+start_mount() {
+	if [ ! -f "$chroot_path/proc/uptime" ]; then
 		busybox mount  /proc  $chroot_path/proc
         fi
 
         if [ ! -f "$chroot_path/dev/random" ]; then
-                #mount --bind /dev $chroot_path/dev
 	        busybox mount /dev $chroot_path/dev
-               # mount --bind /dev/pts $chroot_path/dev/pts
 	        busybox mount /dev/pts $chroot_path/dev/pts
 		ln -n /dev/graphics/fb0 /dev/fb0
         fi
 
         if [ ! -d "$chroot_path/sys/kernel" ]; then
-                #mount --bind /sys $chroot_path/sys
                 busybox mount /sys $chroot_path/sys
         fi
+	}
 
- #       if [ ! -d "$chroot_path/dev/pts" ]; then
- #               mount --bind /dev/pts $chroot_path/dev/pts
- #       fi
-	 }
-
-
-        start_mount
-        #chroot $chroot_path /bin/bash "startx &"
-        chroot $chroot_path /bin/bash "startx"
 
 stop_mount() {
 	sleep 2
@@ -56,6 +40,9 @@ stop_mount() {
 	umount $chroot_path
         }
 
+
+        start_mount
+        chroot $chroot_path /bin/bash "startx"
 
         stop_mount
         setprop ctl.start media & setprop ctl.start zygote & setprop ctl.start surfaceflinger & setprop ctl.start drm
