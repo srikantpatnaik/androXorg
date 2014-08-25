c75cbb0de4591cb3d6ba244898f83b3cdb3ea05e
Srikant Patnaik <srikant@gnu-linux.org>
2014-08-25 12:45:30 +0530

    stop mount and restart support

--- androlinux_xorg.sh
+++ androlinux_xorg.sh
@@ -46,4 +46,16 @@ start_mount()
         start_mount
         chroot $chroot_path /bin/bash "startx &"
 
+stop_mount() {
+        umount $chroot_path/proc
+        umount $chroot_path/dev/pts
+        umount $chroot_path/dev
+        umount $chroot_path/sys
+        }
+
+
+        stop_mount
+        setprop ctl.start media & setprop ctl.start zygote & setprop ctl.start surfaceflinger & setprop ctl.start drm
+
+
 # mount -o rw,remount /dev/block/mtdblock9 /system
